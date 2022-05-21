cfa49d56919b1e1485a2135a71c6228fafefcf40
Ambili das v s <ambili.kan@gmail.com>
2022-05-21 14:21:21 +0530

    Program for return maximum float value

--- IntegerMax.cs
+++ IntegerMax.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TestMaximum
 {
-    internal class IntegerMax
+    public class IntegerMax
     {
         public static int MaxIntNumber(int firstnum,int secondnum,int thirdnum)
         {
@@ -26,4 +26,25 @@ namespace TestMaximum
              return firstnum;
         }
     }
+    public class FloatMax
+    {
+        public static float MaxFloatNumber(float firstnum, float secondnum, float thirdnum)
+        {
+            if (firstnum.CompareTo(secondnum) > 0 && firstnum.CompareTo(thirdnum) > 0 || firstnum.CompareTo(secondnum) >= 0 && firstnum.CompareTo(thirdnum) > 0 || firstnum.CompareTo(secondnum) > 0 && firstnum.CompareTo(thirdnum) >= 0)
+            {
+                return firstnum;
+            }
+            if (secondnum.CompareTo(firstnum) > 0 && secondnum.CompareTo(thirdnum) > 0 || secondnum.CompareTo(firstnum) >= 0 && secondnum.CompareTo(thirdnum) > 0 || secondnum.CompareTo(firstnum) > 0 && secondnum.CompareTo(thirdnum) >= 0)
+            {
+                return secondnum;
+            }
+            if (thirdnum.CompareTo(firstnum) > 0 && thirdnum.CompareTo(secondnum) > 0 || thirdnum.CompareTo(firstnum) >= 0 && thirdnum.CompareTo(secondnum) > 0 || thirdnum.CompareTo(firstnum) > 0 && thirdnum.CompareTo(secondnum) >= 0)
+            {
+                return thirdnum;
+            }
+
+            return firstnum;
+        }
+    }
+   
 }
--- Program.cs
+++ Program.cs
@@ -7,9 +7,12 @@ namespace TestMaximum
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Test Maximum Program");
-            Console.WriteLine(IntegerMax.MaxIntNumber(20,20,5));
+            Console.WriteLine(IntegerMax.MaxIntNumber(20,10,5));
             Console.WriteLine(IntegerMax.MaxIntNumber(5, 20, 10));
-            Console.WriteLine(IntegerMax.MaxIntNumber(5,10,20)); 
+            Console.WriteLine(IntegerMax.MaxIntNumber(5,10,20));
+            Console.WriteLine(FloatMax.MaxFloatNumber(5.5f,3.2f,1.2f));
+            Console.WriteLine(FloatMax.MaxFloatNumber(3.2f,5.5f,1.2f));
+            Console.WriteLine(FloatMax.MaxFloatNumber(3.2f,1.2f,5.5f));
         }
     }
 }
