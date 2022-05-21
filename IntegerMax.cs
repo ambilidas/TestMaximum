7fee5693ceb93ecde92e92aa0b6825579a737e85
Ambili das v s <ambili.kan@gmail.com>
2022-05-21 13:53:56 +0530

    Program for return the max integer value

---
+++ IntegerMax.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMaximum
+{
+    internal class IntegerMax
+    {
+        public static int MaxIntNumber(int firstnum,int secondnum,int thirdnum)
+        {
+            if(firstnum.CompareTo(secondnum) >  0 && firstnum.CompareTo(thirdnum) > 0)
+            {
+                return firstnum;
+            }
+            else if(secondnum.CompareTo(firstnum) > 0 && secondnum.CompareTo(thirdnum) > 0)
+            {
+                return secondnum;
+            }
+             return thirdnum;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ namespace TestMaximum
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Test Maximum Program");
+            Console.WriteLine(IntegerMax.MaxIntNumber(20,10,5));
+            Console.WriteLine(IntegerMax.MaxIntNumber(5, 20, 10));
+            Console.WriteLine(IntegerMax.MaxIntNumber(5,10,20)); 
         }
     }
 }
