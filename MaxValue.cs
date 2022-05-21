fe3358cb04ec61a3b5745ca92b19979b57a3087e
Ambili das v s <ambili.kan@gmail.com>
2022-05-21 14:40:00 +0530

    Program for return max string value

--- MaxValue.cs
+++ MaxValue.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TestMaximum
 {
-    public class IntegerMax
+    public class MaxValue
     {
         public static int MaxIntNumber(int firstnum,int secondnum,int thirdnum)
         {
@@ -44,6 +44,26 @@ namespace TestMaximum
             }
 
             return firstnum;
+        }
+    }
+    public class StringMax
+    {
+        public static string MaxStringValue(string first,string second,string third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0 || first.CompareTo(second) >= 0 && first.CompareTo(third) > 0 || first.CompareTo(second) > 0 && first.CompareTo(third) >= 0)
+            {
+                return first;
+            }
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0 || second.CompareTo(first) >= 0 && second.CompareTo(third) > 0 || second.CompareTo(first) > 0 && second.CompareTo(third) >= 0)
+            {
+                return second;
+            }
+            if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0 || third.CompareTo(first) >= 0 && third.CompareTo(second) > 0 || third.CompareTo(first) > 0 && third.CompareTo(second) >= 0)
+            {
+                return third;
+            }
+
+            return first;
         }
     }
    
--- Program.cs
+++ Program.cs
@@ -7,12 +7,15 @@ namespace TestMaximum
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Test Maximum Program");
-            Console.WriteLine(IntegerMax.MaxIntNumber(20,10,5));
-            Console.WriteLine(IntegerMax.MaxIntNumber(5, 20, 10));
-            Console.WriteLine(IntegerMax.MaxIntNumber(5,10,20));
+            Console.WriteLine(MaxValue.MaxIntNumber(20,10,5));
+            Console.WriteLine(MaxValue.MaxIntNumber(5, 20, 10));
+            Console.WriteLine(MaxValue.MaxIntNumber(5,10,20));
             Console.WriteLine(FloatMax.MaxFloatNumber(5.5f,3.2f,1.2f));
             Console.WriteLine(FloatMax.MaxFloatNumber(3.2f,5.5f,1.2f));
             Console.WriteLine(FloatMax.MaxFloatNumber(3.2f,1.2f,5.5f));
+            Console.WriteLine(StringMax.MaxStringValue("Bridgelabzzz","Bridgelabzz","Bridgelabz"));
+            Console.WriteLine(StringMax.MaxStringValue("Bridgelabzz", "Bridgelabzzz", "Bridgelabz"));
+            Console.WriteLine(StringMax.MaxStringValue("Bridgelabz", "Bridgelabzz", "Bridgelabzzz"));
         }
     }
 }
